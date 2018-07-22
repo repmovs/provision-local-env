95c988a6d2cc3ae866de83c86ffe8259934c5168
Repmovs <repmovs@gmail.com>
2018-07-22 12:54:38 +0300

    Added initial setup and config, no development yet

---
+++ README.md
@@ -0,0 +1,3 @@
+Install With One Command
+
+wget -qO- https://github.com/repmovs/provision-local-env/raw/master/run.sh | bash
---
+++ run.sh
@@ -0,0 +1,13 @@
+sudo zypper install python3-setuptools git
+sudo pip install ansible
+
+cd ~
+mkdir -p setup
+cd setup
+git clone https://github.com/repmovs/provision-local-env.git
+cd provision-local-env
+
+ansible-playbook setup.yml -i HOSTS --ask-sudo-pass
+
+source ~/.bashrc
+exit 0
